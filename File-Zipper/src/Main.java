8c4f533bc4d7880c15f8dc954c9fef48f46d2c25
LazarBajkic <113618364+LazarBajkic@users.noreply.github.com>
2023-04-23 19:39:52 +0200

    File zip

---
+++ File-Zipper/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        String path1=s.nextLine();
+        String path2=s.nextLine();
+        Compress(path1, path2);
+    }
+
+    public static void Compress(String path1,String path2) throws IOException{
+        FileOutputStream fos = new FileOutputStream(path2);
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+
+        File fileToZip = new File(path1);
+        FileInputStream fis = new FileInputStream(fileToZip);
+        ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+        zipOut.putNextEntry(zipEntry);
+
+        byte[] bytes = new byte[1024];
+        int length;
+        while((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
+
+        zipOut.close();
+        fis.close();
+        fos.close();
+    }
+   
+}
