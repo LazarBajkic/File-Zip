4ee3c4bc4c702cba32fbd31a314728a0a3f66734
LazarBajkic <113618364+LazarBajkic@users.noreply.github.com>
2023-05-02 10:13:43 +0200

    File Zipper

--- File-Zipper/src/Main.java
+++ File-Zipper/src/Main.java
@@ -35,5 +35,24 @@ public class Main {
         fis.close();
         fos.close();
     }
+
+    public static void Decompress(String path1,String path2) throws IOException{
+        FileOutputStream fos = new FileOutputStream(path2);
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+        File fiuz = new File(path1);
+        FileInputStream fis = new FileInputStream(fiuz);
+        ZipEntry zipEntry = new ZipEntry(fiuz.getName());
+        zipOut.putNextEntry(zipEntry);
+
+        byte[] bytes = new byte[1024];
+        int length;
+        while((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
+
+        zipOut.close();
+        fis.close();
+        fos.close();
+    }
    
 }
